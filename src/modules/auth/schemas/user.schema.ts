151c6f6f149d8a9a795dd850261aa69aed68018b
Leonardo Alves <leonardo.silva@pceembalagens.com.br>
2026-09-15 08:37:08 -0400

    Refactor User schema to use definite assignment assertions for properties. Updated name, email, password, role, and active fields to ensure they are initialized before use.

--- src/modules/auth/schemas/user.schema.ts
+++ src/modules/auth/schemas/user.schema.ts
@@ -18,7 +18,7 @@ export class User {
     required: true,
     trim: true
   })
-  name: string;
+  name!: string;
 
   @Prop({
     required: true,
@@ -26,23 +26,23 @@ export class User {
     lowercase: true,
     trim: true
   })
-  email: string;
+  email!: string;
 
   @Prop({
     required: true
   })
-  password: string;
+  password!: string;
 
   @Prop({
     enum: UserRole,
     default: UserRole.USER
   })
-  role: UserRole;
+  role!: UserRole;
 
   @Prop({
     default: true
   })
-  active: boolean;
+  active!: boolean;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
